5aa49cec82639862e6718d2b09e051e7dad1c4ef
huju <hujudev@gmail.com>
2023-05-22 23:46:43 +0200

    change command line arguments to options instead of being determined by amount of arguments (sentence option WIP)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 from classes.image_generator import ImageGenerator
 from classes.image_encrypting import VisualCryptography
 from classes.image_transparency import ImageTransparency
@@ -10,75 +11,45 @@ MAX_TEXT_LENGTH = 10
 
 
 def main():
-    image_size = (50, 50)
-    if len(sys.argv) < 2:
+    parser = argparse.ArgumentParser(description='Image encryption tool')
+    parser.add_argument('-t', '--text', help='The text to encrypt', default=None)
+    parser.add_argument('-st', '--sentence', help='The sentence to encrypt', default=None)
+    parser.add_argument('-s', '--size', help='Image size: small or large', default="small")
+    parser.add_argument('-f', '--file', help='The private key file', default=None)
+
+    args = parser.parse_args()
+
+    if args.text and args.sentence:
+        print("Error: Please provide either a text or a sentence, not both.")
+        return
+    elif args.text:
+        text_input = args.text
+    elif args.sentence:
+        text_input = args.sentence
+    else:
         text_input = input("Enter the text to encrypt (max 10 characters): ")
-        if len(text_input) > MAX_TEXT_LENGTH:
-            print("Error: Input text should be up to 10 characters.")
-            return
-        image_size_option = input("Select small/large image size (small if not specified)")
-        if image_size_option.lower() == "small":
-            image_size = (50, 50)
-            scale_factor = 4
-        elif image_size_option.lower() == "large":
-            image_size = (100, 100)
-            scale_factor = 2
-        elif not image_size_option:
-            image_size_option = "small"
-            image_size = (50, 50)
-            scale_factor = 4
-        else:
-            print("Invalid image size option.")
-            return
-        private_key_filename = f"default_{image_size[0]}.png"
-        private_key_filename = input(f"Enter the private key filename ({private_key_filename} if not specified): ")
-        if not private_key_filename:
-            private_key_filename = f"default_{image_size[0]}.png"
-    elif len(sys.argv) == 2:
-        text_input = sys.argv[1]
-        if len(text_input) > MAX_TEXT_LENGTH:
-            print("Error: Input text should be up to 10 characters.")
-            return
+
+    if len(text_input) > MAX_TEXT_LENGTH:
+        print("Error: Input text should be up to 10 characters.")
+        return
+
+    if args.size.lower() == "small":
         image_size_option = "small"
         image_size = (50, 50)
         scale_factor = 4
-        private_key_filename = f"default_50.png"
-    elif len(sys.argv) == 3:
-        text_input = sys.argv[1]
-        if len(text_input) > MAX_TEXT_LENGTH:
-            print("Error: Input text should be up to 10 characters.")
-            return
-        image_size_option = sys.argv[2]
-        if image_size_option.lower() == "small":
-            image_size = (50, 50)
-            scale_factor = 4
-        elif image_size_option.lower() == "large":
-            image_size = (100, 100)
-            scale_factor = 2
-        else:
-            print("Invalid image size option.")
-            return
-        private_key_filename = f"default_{image_size[0]}.png"
-    elif len(sys.argv) == 4:
-        text_input = sys.argv[1]
-        if len(text_input) > MAX_TEXT_LENGTH:
-            print("Error: Input text should be up to 10 characters.")
-            return
-        image_size_option = sys.argv[2]
-        if image_size_option.lower() == "small":
-            image_size = (50, 50)
-            scale_factor = 4
-        elif image_size_option.lower() == "large":
-            image_size = (100, 100)
-            scale_factor = 2
-        else:
-            print("Invalid image size option.")
-            return
-        private_key_filename = sys.argv[3]
+    elif args.size.lower() == "large":
+        image_size_option = "large"
+        image_size = (100, 100)
+        scale_factor = 2
+    else:
+        print("Invalid image size option.")
+        return
 
+    private_key_filename = args.file
+    if not private_key_filename:
+        private_key_filename = f"default_{image_size[0]}.png"
     private_key_path = f"private_keys/{private_key_filename}"
     private_key_extension = os.path.splitext(private_key_filename)[1]
-
     if private_key_extension.lower() != ".png":
         print("Private key file should be in PNG format.")
         return
@@ -88,7 +59,7 @@ def main():
     img_gen.create_image()
 
     # Prepare the image for encryption
-    image_suffix = "_small" if image_size_option.lower() == "small" else "_large"
+    image_suffix = "_small" if image_size_option == "small" else "_large"
     message_image = f"messages/{text_input}{image_suffix}/message_{text_input}{image_suffix}.png"
     vc = VisualCryptography(message=text_input, message_image=message_image, private_key=private_key_path,
                             image_suffix=image_suffix)
